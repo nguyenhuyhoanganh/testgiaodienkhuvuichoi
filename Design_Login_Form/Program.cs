b77eff7946b167c643fcc22cdeede4fae53fc830
LeThanh <LeThanh@DESKTOP-7HIL2OS>
2021-03-24 10:29:47 +0700

    up

--- Design_Login_Form/Program.cs
+++ Design_Login_Form/Program.cs
@@ -16,7 +16,7 @@ namespace Design_Login_Form
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fQuanLyNhanVien());
+            Application.Run(new fLogin());
         }
     }
 }
